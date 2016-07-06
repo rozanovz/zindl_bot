40247828c7027a5c4b73af6ecd5d8dcefa8cc12d
rozanovz <rozanovz@gmail.com>
2016-07-06 17:31:49 +0300

    save

--- bot/bot.currency.convert.js
+++ bot/bot.currency.convert.js
@@ -5,9 +5,11 @@ var token = '246011169:AAGr0HtrZi6dVXRjlA1aLyyhid_Z0f44sJQ';
 var bot = new TelegramBot(token, {polling: true});
 
 bot.onText(/\/sell (.+)/, function (msg, match) {
+  var fromId = msg.from.id;
   bot.sendMessage(fromId, ConvertCurrency(msg, 'sell'));
 });
 
 bot.onText(/\/buy (.+)/, function (msg, match) {
+  var fromId = msg.from.id;
   bot.sendMessage(fromId, ConvertCurrency(msg, 'buy'));
 });
